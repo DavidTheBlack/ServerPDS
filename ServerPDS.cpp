f08865bed56ce5bc2efdf62242b350771bd6fa78
David <david@freeisland.eu>
2017-03-17 10:24:11 +0100

    Test Uso windowEnum

--- ServerPDS.cpp
+++ ServerPDS.cpp
@@ -2,12 +2,59 @@
 //
 
 #include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+#include <string>
 
 
-int main()
-{
-	printf("Fuck the system");
-	printf("Hello world");
-    return 0;
+BOOL CALLBACK enumWindowsProc(
+	__in  HWND hWnd,
+	__in  LPARAM lParam
+) {
+	if (IsWindowVisible(hWnd)) {
+		int length = GetWindowTextLength(hWnd);
+		if (length != 0) {
+			TCHAR* buffer;
+			buffer = new TCHAR[length + 1];
+			memset(buffer, 0, (length + 1) * sizeof(char));
+
+			GetWindowText(hWnd, buffer, length + 1);
+			std::wstring windowTitle = std::wstring(buffer);
+			delete[] buffer;
+			std::wcout << hWnd << TEXT(": ") << windowTitle << std::endl;
+		}
+		else {
+			std::wcout << hWnd << TEXT(": Finestra senza titolo") << std::endl;
+		}
+		return true;
+	}
+	else {
+		ShowWindow(hWnd, SW_SHOW);
+		//std::wcout << hWnd << TEXT(": Icona") << std::endl;
+		return true;
+	}
+	
+	/*if (!IsIconic(hWnd)) {
+		int length = GetWindowTextLength(hWnd);
+		//if (0 == length) return TRUE;
+
+		TCHAR* buffer;
+		buffer = new TCHAR[length + 1];
+		memset(buffer, 0, (length + 1) * sizeof(char));
+
+		GetWindowText(hWnd, buffer, length + 1);
+		std::wstring windowTitle = std::wstring(buffer);
+		delete[] buffer;
+
+		std::wcout << hWnd << TEXT(": ") << windowTitle << std::endl;
+		*/
+	}
+	
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	std::wcout << TEXT("Enumerating Windows...") << std::endl;
+	BOOL enumeratingWindowsSucceeded = EnumWindows(enumWindowsProc, NULL);
+	std::cin.get();
+	return 0;
+	
 }
-
