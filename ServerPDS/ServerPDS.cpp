4ccfc8cff400ba7e94c4a67669b89685f7a99c59
David <david@freeisland.eu>
2017-03-17 13:45:00 +0100

    Determinata lista applicazioni con gui
    Vengono scartate tutte le finestre che sono popup windod. Non vengono
    considerate le applicazioni in tray e senza finestre

--- ServerPDS/ServerPDS.cpp
+++ ServerPDS/ServerPDS.cpp
@@ -7,48 +7,87 @@
 #include <string>
 
 
+/*	
+	Check if the window identified from hwnd 
+	handler is a visible window that can be 
+	selected with ALT+TAB key combination
+	*/
+BOOL IsAltTabWindow(HWND hwnd)
+{
+		
+		TITLEBARINFO ti;
+		HWND hwndTry, hwndWalk = NULL;
+		
+		//If the window is not visible then is not a window of interest
+		if (!IsWindowVisible(hwnd))
+			return false;
+	
+		
+		//Check and ignore the PopUp and dialog windows 
+		hwndTry = GetAncestor(hwnd, GA_ROOTOWNER);
+		while (hwndTry != hwndWalk)
+		{
+			hwndWalk = hwndTry;
+			//Se la finestra non ha richiamato altre sottofinestre la GetLastActivePopup restituisce 
+			//l'handler della finestra stessa quindi il ciclo si interrompe
+			hwndTry = GetLastActivePopup(hwndWalk);
+			if (IsWindowVisible(hwndTry))
+				break;
+		}
+
+		//If hwnd is an handler from a "root" window (ancestor) the analysis proceeds
+		//otherwise the window is ignored
+		if (hwndWalk != hwnd)
+			return false;
+		
+
+		// Get the title bar information for current window and check if is invisible
+		// in this way removes some task tray programs and "Program Manager"
+		ti.cbSize = sizeof(ti);
+		GetTitleBarInfo(hwnd, &ti);
+		if (ti.rgstate[0] & STATE_SYSTEM_INVISIBLE) {
+			return false;
+		}
+			
+		// Check if the window is a ToolWindow -> ALT+SPACE or Right Click Menu 
+		// Tool windows should not be displayed either - these do not appear in the taskbar
+		if (GetWindowLong(hwnd, GWL_EXSTYLE) & WS_EX_TOOLWINDOW) {
+			return false;
+		}
+		//The windows is an active window that can be selected with ALT+TAB
+		return true;
+	}
+
+
 BOOL CALLBACK enumWindowsProc(
 	__in  HWND hWnd,
 	__in  LPARAM lParam
 ) {
-	if (IsWindowVisible(hWnd)) {
-		int length = GetWindowTextLength(hWnd);
-		if (length != 0) {
-			TCHAR* buffer;
-			buffer = new TCHAR[length + 1];
-			memset(buffer, 0, (length + 1) * sizeof(char));
+		if (IsAltTabWindow(hWnd)) {
+			int length = GetWindowTextLength(hWnd);
+			if (length != 0) {
+				TCHAR* buffer;
+				buffer = new TCHAR[length + 1];
+				memset(buffer, 0, (length + 1) * sizeof(char));
 
-			GetWindowText(hWnd, buffer, length + 1);
-			std::wstring windowTitle = std::wstring(buffer);
-			delete[] buffer;
-			std::wcout << hWnd << TEXT(": ") << windowTitle << std::endl;
+				GetWindowText(hWnd, buffer, length + 1);
+				std::wstring windowTitle = std::wstring(buffer);
+				delete[] buffer;
+				std::wcout << hWnd << TEXT(": ") << windowTitle << std::endl;
+				
+
+			}
+			else {
+				std::wcout << hWnd << TEXT(": Finestra senza titolo") << std::endl;
+			}
 		}
-		else {
-			std::wcout << hWnd << TEXT(": Finestra senza titolo") << std::endl;
-		}
-		return true;
-	}
-	else {
-		//ShowWindow(hWnd, SW_SHOW);
-		//std::wcout << hWnd << TEXT(": Icona") << std::endl;
-		return true;
-	}
+
+		
 	
-	/*if (!IsIconic(hWnd)) {
-		int length = GetWindowTextLength(hWnd);
-		//if (0 == length) return TRUE;
+	return true;
+}
 
-		TCHAR* buffer;
-		buffer = new TCHAR[length + 1];
-		memset(buffer, 0, (length + 1) * sizeof(char));
-
-		GetWindowText(hWnd, buffer, length + 1);
-		std::wstring windowTitle = std::wstring(buffer);
-		delete[] buffer;
-
-		std::wcout << hWnd << TEXT(": ") << windowTitle << std::endl;
-		*/
-	}
+	
 	
 
 int _tmain(int argc, _TCHAR* argv[]) {
